89c36325db7d0cd6faaa311c53898d088978f87f
Marius Kristiansen <mariussk@getacademy.no>
2021-01-26 09:07:32 +0100

    .

---
+++ Student-06a-InheritanceInterfaces/LibrarySystem/LibrarySystem/ReservableDVD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LibrarySystem.Item;
+
+namespace LibrarySystem
+{
+    class ReservableDVD : DVD, IReservable
+    {
+        public Member ReservedTo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Reserved { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ReservableDVD(string title, int playTime, Classification rating) : base(title, playTime, rating)
+        {
+        }
+
+
+        public bool CanBeReservedFor(Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReserveItemFor(Member member)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
