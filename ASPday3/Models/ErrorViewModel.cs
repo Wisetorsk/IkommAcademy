2608e93c87b1baa1a058efb837130b3e06531011
Marius Kristiansen <thewisetorsk@gmail.com>
2021-02-03 08:43:12 +0100

    Initial upload

---
+++ ASPday3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPday3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
